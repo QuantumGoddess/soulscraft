27834cdee4aa8e2cf3b89e6a5c7796f7aba49b98
QuantumGoddess <ambervanraak@gmail.com>
2023-02-23 15:43:11 +0100

    Added creeper explosion radius increase

--- src/main/java/net/quantumgoddess/soulscraft/mixin/CreeperEntityMixin.java
+++ src/main/java/net/quantumgoddess/soulscraft/mixin/CreeperEntityMixin.java
@@ -1,27 +1,21 @@
 package net.quantumgoddess.soulscraft.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.entity.attribute.DefaultAttributeContainer;
-import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.CreeperEntity;
-import net.minecraft.entity.mob.HostileEntity;
 
 @Mixin(CreeperEntity.class)
-public class CreeperEntityMixin {
+public abstract class CreeperEntityMixin {
+    @Accessor
+    public abstract void setExplosionRadius(int explosionRadius);
 
-	private static double movementSpeed = 0.0;
-	private static double attackDamage = 0.0;
-	private static double armor = 4.0;
-	private static double health = 40.0;
-	private static double followRange = 70.0;
+    @Inject(at = @At("TAIL"), method = "<init>(Lnet/minecraft/entity/EntityType;Lnet/minecraft/world/World;)V")
+    private void CreeperEntity(CallbackInfo info) {
+        ((CreeperEntityMixin) (Object) this).setExplosionRadius(5);
+    }
 
-	@Inject(at = @At("HEAD"), method = "createCreeperAttributes()Lnet/minecraft/entity/attribute/DefaultAttributeContainer$Builder;", cancellable = true)
-	private static void createCreeperAttributes(CallbackInfoReturnable<DefaultAttributeContainer.Builder> info) {
-		info.setReturnValue(
-				HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MOVEMENT_SPEED, movementSpeed));
-	}
 }
