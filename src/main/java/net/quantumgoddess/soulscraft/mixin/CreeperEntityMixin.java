2f1e3625adf9568ee4633c69a58c3256122a51b4
QuantumGoddess <ambervanraak@gmail.com>
2023-02-23 16:18:54 +0100

    changed inject to shadow

--- src/main/java/net/quantumgoddess/soulscraft/mixin/CreeperEntityMixin.java
+++ src/main/java/net/quantumgoddess/soulscraft/mixin/CreeperEntityMixin.java
@@ -1,21 +1,13 @@
 package net.quantumgoddess.soulscraft.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
+import org.spongepowered.asm.mixin.Shadow;
 import net.minecraft.entity.mob.CreeperEntity;
 
 @Mixin(CreeperEntity.class)
 public abstract class CreeperEntityMixin {
-    @Accessor
-    public abstract void setExplosionRadius(int explosionRadius);
 
-    @Inject(at = @At("TAIL"), method = "<init>(Lnet/minecraft/entity/EntityType;Lnet/minecraft/world/World;)V")
-    private void CreeperEntity(CallbackInfo info) {
-        ((CreeperEntityMixin) (Object) this).setExplosionRadius(5);
-    }
+    @Shadow
+    private int explosionRadius = 5;
 
 }
