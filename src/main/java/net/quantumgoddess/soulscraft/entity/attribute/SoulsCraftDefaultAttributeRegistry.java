4f3ea2a5209f728ac3eb52f87099d8d4f7b094f9
QuantumGoddess <ambervanraak@gmail.com>
2023-02-26 01:17:48 +0100

    added spider and witch attributes

--- src/main/java/net/quantumgoddess/soulscraft/entity/attribute/SoulsCraftDefaultAttributeRegistry.java
+++ src/main/java/net/quantumgoddess/soulscraft/entity/attribute/SoulsCraftDefaultAttributeRegistry.java
@@ -9,37 +9,49 @@ import net.minecraft.entity.mob.HostileEntity;
 
 public class SoulsCraftDefaultAttributeRegistry {
 
-    public static void registerAll() {
-        FabricDefaultAttributeRegistry.register(EntityType.ZOMBIE,
-                SoulsCraftDefaultAttributeRegistry.createZombieAttributes());
-        FabricDefaultAttributeRegistry.register(EntityType.CREEPER,
-                SoulsCraftDefaultAttributeRegistry.createCreeperAttributes());
-        FabricDefaultAttributeRegistry.register(EntityType.ENDERMAN,
-                SoulsCraftDefaultAttributeRegistry.createEndermanAttributes());
-        FabricDefaultAttributeRegistry.register(EntityType.PLAYER,
-                SoulsCraftDefaultAttributeRegistry.createPlayerAttributes());
-    }
+	public static void registerAll() {
+		FabricDefaultAttributeRegistry.register(EntityType.ZOMBIE, createZombieAttributes());
+		FabricDefaultAttributeRegistry.register(EntityType.CREEPER, createCreeperAttributes());
+		FabricDefaultAttributeRegistry.register(EntityType.ENDERMAN, createEndermanAttributes());
+		FabricDefaultAttributeRegistry.register(EntityType.PLAYER, createPlayerAttributes());
+		FabricDefaultAttributeRegistry.register(EntityType.WITCH, createWitchAttributes());
+		FabricDefaultAttributeRegistry.register(EntityType.SPIDER, createSpiderAttributes());
+	}
 
-    private static DefaultAttributeContainer.Builder createZombieAttributes() {
-        return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_FOLLOW_RANGE, 35.0)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.23f).add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 6.0)
-                .add(EntityAttributes.GENERIC_ARMOR, 4.0).add(EntityAttributes.ZOMBIE_SPAWN_REINFORCEMENTS);
-    }
+	public static DefaultAttributeContainer.Builder createSpiderAttributes() {
+		return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 32.0)
+				.add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.3f);
+	}
 
-    private static DefaultAttributeContainer.Builder createCreeperAttributes() {
-        return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.25);
-    }
+	private static DefaultAttributeContainer.Builder createWitchAttributes() {
+		return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 52.0)
+				.add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.25);
+	}
 
-    private static DefaultAttributeContainer.Builder createEndermanAttributes() {
-        return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 80.0)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.3f).add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 14.0)
-                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 64.0);
-    }
+	private static DefaultAttributeContainer.Builder createZombieAttributes() {
+		return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_FOLLOW_RANGE, 35.0)
+				.add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.23f)
+				.add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 6.0)
+				.add(EntityAttributes.GENERIC_ARMOR, 4.0)
+				.add(EntityAttributes.ZOMBIE_SPAWN_REINFORCEMENTS);
+	}
 
-    private static DefaultAttributeContainer.Builder createPlayerAttributes() {
-        return LivingEntity.createLivingAttributes().add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 1.0)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.1f).add(EntityAttributes.GENERIC_ATTACK_SPEED, 1.0)
-                .add(EntityAttributes.GENERIC_LUCK)
-                .add(EntityAttributes.GENERIC_MAX_HEALTH, 10.0);
-    }
+	private static DefaultAttributeContainer.Builder createCreeperAttributes() {
+		return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.25);
+	}
+
+	private static DefaultAttributeContainer.Builder createEndermanAttributes() {
+		return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 80.0)
+				.add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.3f)
+				.add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 14.0)
+				.add(EntityAttributes.GENERIC_FOLLOW_RANGE, 64.0);
+	}
+
+	private static DefaultAttributeContainer.Builder createPlayerAttributes() {
+		return LivingEntity.createLivingAttributes().add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 1.0)
+				.add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.1f)
+				.add(EntityAttributes.GENERIC_ATTACK_SPEED, 1.0)
+				.add(EntityAttributes.GENERIC_LUCK)
+				.add(EntityAttributes.GENERIC_MAX_HEALTH, 10.0);
+	}
 }
